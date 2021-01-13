fe43551cc7fb39b0db9d1cefd7aa697025b1f61c
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-01-13 13:16:12 +0100

    juan

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\user;
 
 class UsuarioController extends Controller
 {
     public function accesoGestion(){
 
         $datos['users']=User::paginate(5);
+
         return view('acciones.gestion', $datos);
     }
 }
--- resources/views/acciones/gestion.blade.php
+++ resources/views/acciones/gestion.blade.php
@@ -1,20 +1,20 @@
-<form action="" method="post" enctype="multipart/form-data">
-<h1>Gestion de usuarios</h1>
-
-<table class= "table table-light table-hover">
-<thead class="thead-light">
+<h1>Gestión de usuarios</h1>
+<table class="table table-light table-hover">
+    <thead class="thead-light">
         <tr>
             <th>ID</th>
             <th>Name</th>
-            <th>surname</th>
+            <th>email</th>
         </tr>
     </thead>
 
-<tbody>
-@foreach ($users as $user)
-<tr></tr>
-<td>{{$user->id}}</td> 
-<td>{{$user->name}}</td>
-<td>{{$user->firstname}}</td>
-</tbody>
-</form>
+    <tbody>
+        @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
