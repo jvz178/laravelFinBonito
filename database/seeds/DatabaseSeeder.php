5762698098c0738307eefd6fe222744080d2e553
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-02-03 13:11:34 +0100

    j

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         factory(\App\Ciclo::class, 5)->create();
         factory(\App\Noticia::class, 25)->create();
-        factory(\App\Oferta::class, 30)->create();
+        factory(\App\Oferta::class, 3)->create();
         factory(\App\Requisito::class, 10)->create();
         factory(\App\User::class, 50)->create();
         factory(\App\Aplica::class, 30)->create();
