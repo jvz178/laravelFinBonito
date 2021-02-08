a9a3856430760a16071623151ae10c96f9cbdbfe
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-02-08 18:26:53 +0100

    j

--- routes/web.php
+++ routes/web.php
@@ -18,31 +18,12 @@ Route::get('/', function () {
 Route::group(['middleware' => 'auth'], function () {
 // Route::group(['middleware' => 'admin'], function () {
 
-<<<<<<< HEAD
-Route::get('gestion', 'UsuarioController@accesoGestion')->name("gestion");
-//Route::get('gestion', 'UsuarioController@accesoGestion2')->name("gestion2");
-Route::get('filtro', 'UsuarioController@accesoGestionFiltrado');
-Route::get('filtro2', 'UsuarioController@accesoGestionFiltrado2');
-Route::get('usuario/{id}','UsuarioController@cambiar');
-Route::get('vistaEmail','EmailController@accederEmail');
-Route::post('vistaEmail/enviarEmail', 'EmailController@enviarEmail')->name('enviar');
-Route::get('Error','EmailController@mostrarError');
-Route::get('informes', 'InformesController@general');
-Route::get('informes/ofertas', 'InformesController@informeOferta')->name('pdf');
-// Route::get('informes/oferta2', 'InformesController@informeAlumnos')->name('pdf');
-Route::get('informes/oferta4', 'InformesController@listadoAlumnos')->name('pdf');
-Route::get('informes/oferta3', 'InformesController@ofertasCiclo')->name('pdf');
-Route::resource('/noticias', 'NoticiaController');
-//Rutas pruebas
-Route::get('/users', 'UsuarioController@index');
-//
-=======
-    Route::get('/acciones/gestion', 'UsuarioController@accesoGestion'); 
-    Route::get('/acciones/gestion/filtro', 'UsuarioController@accesoGestionFiltrado');
-    Route::get('/acciones/gestion/filtro2', 'UsuarioController@accesoGestionFiltrado2');
+    Route::get('gestion', 'UsuarioController@accesoGestion'); 
+    Route::get('filtro', 'UsuarioController@accesoGestionFiltrado');
+    Route::get('filtro2', 'UsuarioController@accesoGestionFiltrado2');
     Route::get('usuario/{id}','UsuarioController@cambiar');
-    Route::get('acciones/vistaEmail','EmailController@accederEmail');
-    Route::post('acciones/vistaEmail/enviarEmail', 'EmailController@enviarEmail')->name('enviar');
+    Route::get('vistaEmail','EmailController@accederEmail');
+    Route::post('vistaEmail/enviarEmail', 'EmailController@enviarEmail')->name('enviar');
     Route::get('informes', 'InformesController@general');
     Route::get('informes/ofertas', 'InformesController@informeOferta')->name('pdf');
     // Route::get('informes/oferta2', 'InformesController@informeAlumnos')->name('pdf');
@@ -54,6 +35,5 @@ Route::get('/users', 'UsuarioController@index');
 
 });
 
->>>>>>> 740ad035d45746fd34d71a190e77882cdc24b1ff
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
